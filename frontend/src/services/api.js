e10f4f3bc798e92f8832a70735b4041e49c70757
shubhashish-26 <shubhashishmohanty95@gmail.com>
2026-07-14 13:29:26 +0530

    Fix API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,16 +1,11 @@
 const API_URL = "https://mohanty95.pythonanywhere.com";
 
 export async function getHospitals() {
-
-    const response = await fetch(`${API}/hospitals`);
-
-    return await response.json();
-
+  const response = await fetch(`${API_URL}/hospitals`);
+  return await response.json();
 }
+
 export async function getStats() {
-
-    const response = await fetch(`${API}/stats`);
-
-    return await response.json();
-
+  const response = await fetch(`${API_URL}/stats`);
+  return await response.json();
 }
